7e563e3e75085a49cf50867373a122d8620897fb
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-02-14 15:23:27 +0100

    add: code question 2

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 // Code Question 1
-
 // Entrambi i console.log stamperanno 'Double Cheese Burger'.
 // E' stato creato un solo oggetto.
+
+// Code Question 2
+// Entrambi i console.log stamperanno "Salad".
+// Sono stati creati due oggetti.
+
