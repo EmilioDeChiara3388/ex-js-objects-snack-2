ab66131f723a79a90d4568ff154a7edac9dea5dc
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2025-02-14 15:36:18 +0100

    add: code question 4

--- main.js
+++ main.js
@@ -8,3 +8,7 @@
 
 // Code Question 3
 // Sono stati creati 3 oggetti.
+
+// Code Question 4
+// Per clonare l'oggetto chef, la scelta migliore è utilizzare lo spread(...), in quanto non ci sono oggetti annidati (che non verrebero copiati), ma invece è presente una funzione, che invece verrà copiata.
+// Per clonare l'oggetto restaurant, la scelta migliore è utilizzare structuredClone(), per poter copiare correttamente anche l'oggetto annidato (address), e, al contrario di JSON.parse(JSON.stringify()), structuredClone() manterrebbe il valore di openingDate nel formato corretto.
